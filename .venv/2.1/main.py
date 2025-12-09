8ae55e0f170a3c33e90ab4e95de6c405a2b9e4c3
kornel <gg@g.k>
2025-12-09 13:41:49 +0100

    2.3.2

---
+++ .venv/2.1/main.py
@@ -0,0 +1,9 @@
+print('Hello '+ " world!", 1 + 5, "\nto jest w nowej linijce") ###mozna dodac argumenty wpisujac je po przecinku,
+
+print("hello","world","in","2025", sep="-")
+
+print("---------------------2.1.12-----------------------------\n")
+
+print("\nProgramming", "Essentials", "in", sep="***", end="...")
+print("Python")
+
---
+++ .venv/2.2/2.2.py
@@ -0,0 +1,16 @@
+# print("osemkowy", 0o123)
+# print("szesnastkowy", 0x123)
+#
+# print(4.)
+# print(.4)
+#
+# print(3e4)
+# print(0.2e10)
+
+
+print("---------------------2.2.4-----------------------------\n")
+print("I'm Monty Python\nlub")
+print('I\'m  Monty Python')
+
+print("---------------------2.2.6-----------------------------\n")
+print('"I\'m"\n""learning""\n"""Python"""')
