2183208e5d55d457ce5a0d0cb6cc369a014a667d
kornel <gg@g.k>
2025-12-10 08:50:51 +0100

    zaczynać od 3.1

---
+++ .venv/2.4/2.4.py
@@ -0,0 +1,30 @@
+# a = 3.0
+# b = 4.0
+# c = (a ** 2 + b ** 2)
+# print("c =", c)
+#
+print("------------------------------------2.4.7-----------------------------------------")
+john = 2
+mary = 3
+adam = 6
+print(john, mary, adam, sep=', ')
+total_apples = john + mary + adam
+print(total_apples)
+
+print("------------------------------------2.4.9-----------------------------------------")
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.6
+kilometers_to_miles = kilometers / 1.6
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
+print("------------------------------------2.4.10-----------------------------------------")
+x =  1
+x = float(x)
+y = 3 * x ** 1 - 2 * x ** 2 + 3 * x - 1
+print("y =", y)
+
+print("------------------------------------2.4.10-----------------------------------------")
---
+++ .venv/2.6/2.6.py
@@ -0,0 +1,23 @@
+# liczba_1 = int(input("Podaj liczbe 1: "))
+# liczba_2 = int(input("Podaj liczbe 2: "))
+# print("Suma 2 liczb podanych przez uzytkownika: ", liczba_1 + liczba_2)
+
+print("------------------------------------2.6.9-----------------------------------------")
+# a = int(input("Podaj liczbe a: "))
+# b = int(input("Podaj liczbe b: "))
+#
+# print("Dodawanie a do b",a + b)
+# print("Odejmowanie a od b",b - a)
+# print("Mnożenie a razy b",a * b)
+# print("Dzielenie a przez b",a / b)
+
+print("------------------------------------2.6.10-----------------------------------------")
+# x = 1
+# print(1/(x+(1/x+(1/x+(1/x)))))
+
+print("------------------------------------2.6.11-----------------------------------------")
+x = int(input())
+y = int(input())
+
+print(x + y)
+
